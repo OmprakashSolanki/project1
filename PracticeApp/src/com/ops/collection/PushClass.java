b3739ae788fc0a24b10b03b00ea5fb435570cbb6
OmprakashSolanki <solanki.omrakash24@gmail.com>
2019-07-03 00:08:56 +0530

    Second SOP Added

--- PracticeApp/src/com/ops/collection/PushClass.java
+++ PracticeApp/src/com/ops/collection/PushClass.java
@@ -3,5 +3,6 @@ package com.ops.collection;
 public class PushClass {
 public static void main(String[] args) {
 	System.out.println("Hello Git Project");
+	System.out.println("Hello Git added SOP");
 }
 }
